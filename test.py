4485a5019e7394b70ca52b8193712f50e60272ef
Methusan105 <methusan@hotmail.no>
2024-06-22 23:34:29 +0200

    1212

--- test.py
+++ test.py
@@ -19,10 +19,10 @@ def run_workflow(workflow_name, file_number):
     data = {
         "ref": "main",
         "inputs": {
-            "name": f"Wii.Games.zip.{file_number:03d}",
-            "comment": f"test{file_number:03d}",
-            "url": f"https://github.com/Methusan105/Games-and-Programs/releases/download/WG/Wii.Games.zip.{file_number:03d}",
-            "file_name": f"Wii.Games.zip.{file_number:03d}",
+            "name": f"Cyberpunk-{file_number:01d}.bin",
+            "comment": f"test{file_number:01d}",
+            "url": f"https://github.com/Methusan105/Games-and-Programs/releases/download/CP/Cyberpunk-{file_number:01d}.bin",
+            "file_name": f"Cyberpunk-{file_number:01d}.bin",
             "piece_size": "auto",  # Piece size set to auto
             "protocol_version": "hybrid",  # Protocol version set to hybrid
             "maximize_disk_space": "true"  # Assuming you want to maximize disk space
@@ -37,6 +37,6 @@ def run_workflow(workflow_name, file_number):
 
 # Run each workflow for each file
 for workflow_name in workflow_names:
-    for file_number in range(1, 16):
+    for file_number in range(1, ):
         print(f"Running {workflow_name} for file {file_number:03d}")
         run_workflow(workflow_name, file_number)
